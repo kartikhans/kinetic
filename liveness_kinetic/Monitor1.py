da1c4cde3c86ec732f832ffd4467b36cd7e3fc8e
Kartk Hans <kartikhans@tests-MacBook-Pro.local>
2018-12-21 14:22:30 -0500

    minor change

--- liveness_kinetic/Monitor1.py
+++ liveness_kinetic/Monitor1.py
@@ -31,43 +31,40 @@ class Monitor1(DynamicPolicy):
         m=10
         rates=[0,v1,v2,m]
         counter=0
-    def incrementer(self):
-        counter+=1
-        return (counter)
-        ### 1. DEFINE THE LPEC FUNCTION
+    ### 1. DEFINE THE LPEC FUNCTION
     def lpec(f):
-            # Packets with same source IP
-            #  will have a same "state" (thus, same policy applied).
+        # Packets with same source IP
+        #  will have a same "state" (thus, same policy applied).
         h1=f['srcip']
         h2=f['dstip']
         return ( match (srcip=h1 , dstip=h2 ) | match (srcip =h2 , dstip=h1 ) )
-        ### 2. SET UP TRANSITION FUNCTIONS
+    ### 2. SET UP TRANSITION FUNCTIONS
     @transition
     def counter(self):
+        self.counter+=1
         self.case(occured(self.event),self.event)
     @transition
     def policy(self):
-            # If "infected" is True, change policy to "drop"
-                self.case((is_true(V(counter)>=rates[2] and V(counter)<rates[3])),C(drop))
-                self.case((is_true(V(counter)>=rates[2] and V(counter)<rates[3])),C(drop))
-            # Default policy is "indentity", which is "allow".
+        # If "infected" is True, change policy to "drop"
+        self.case((is_true(V(counter)>=rates[2] and V(counter)<rates[3])),C(drop))
+        # Default policy is "indentity", which is "allow".
             self.default(C(fwd()))
-        ### 3. SET UP THE FSM DESCRIPTION
-        
+    ### 3. SET UP THE FSM DESCRIPTION
+
     self.fsm_def =FSMDef(
-                             counter=FSMVar(type=int(),init=counter,trans=counter)
-                             policy=FSMVar(type=Type(Policy,{drop,fwd()}),
-                                           init=fwd(),
-                                           trans=policy))
-                                 ### 4. SET UP POLICY AND EVENT STREAMS
-                                 
-                                 ### This part pretty much remains same for any application
-                                 fsm_pol = FSMPolicy(lpec,self.fsm_def)
-                                 json_event = JSONEvent()
-                                 json_event.register_callback(fsm_pol.event_handler)
-                                           ### This part pretty much remains same for any application
-                                           
-                                           # Specify application class name here. (e.g., "ids")
+                         counter=FSMVar(type=int(),init=counter,trans=counter)
+                         policy=FSMVar(type=Type(Policy,{drop,fwd()}),
+                                       init=fwd(),
+                                       trans=policy))
+                             ### 4. SET UP POLICY AND EVENT STREAMS
+                             
+                             ### This part pretty much remains same for any application
+                             fsm_pol = FSMPolicy(lpec,self.fsm_def)
+                             json_event = JSONEvent()
+                             json_event.register_callback(fsm_pol.event_handler)
+                                       ### This part pretty much remains same for any application
+                                       
+                                       # Specify application class name here. (e.g., "ids")
     super(ids,self).__init__(fsm_pol)
 
 def main():
@@ -80,16 +77,15 @@ def main():
     mc = ModelChecker(smv_str, 'Monitor1')
     
     ## Add specs
-    mc.add_spec("FAIRNESS\n  infected;")
+    mc.add_spec("FAIRNESS\n  counter;")
     
     ### If infected event is true, next policy state is 'drop'
-    mc.add_spec("SPEC AG (infected -> AX policy=drop)")
-    
+    mc.add_spec("SPEC AG (V(counter)>=rates[2] and V(counter)<rates[3]) -> AX policy=drop)")
     ### If infected event is false, next policy state is 'allow'
-    mc.add_spec("SPEC AG (!infected -> AX policy=policy_1)")
+    mc.add_spec("SPEC AG (V(counter)>=0 and V(counter)<=rates[2] -> AX policy=policy_1)")
     
     ### Policy state is 'allow' until infected is true.
-    mc.add_spec("SPEC A [ policy=policy_1 U infected ]")
+    mc.add_spec("SPEC A [ policy=policy_1 U (V(counter)>=rates[2] and V(counter)<rates[3]) ]")
     
     ### It is always possible to go back to 'allow'
     mc.add_spec("SPEC AG EF policy=policy_1")
