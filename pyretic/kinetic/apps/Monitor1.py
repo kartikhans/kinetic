c58cd86d2be746aeaf03ed6e9384c07d4c9b6b59
Kartk Hans <kartikhans@tests-MacBook-Pro.local>
2018-12-26 11:35:43 -0500

    minor changes

--- pyretic/kinetic/apps/Monitor1.py
+++ pyretic/kinetic/apps/Monitor1.py
@@ -26,11 +26,11 @@ from pyretic.kinetic.apps.mac_learner import *
 ### Define a class for the application, subclassed from DynamicPolicy
 class Monitor1(DynamicPolicy):
     count=0
+    v1=2
+    v2=7
+    m=10
+    rates=[0,v1,v2,m]
     def __init__(self):
-        v1=2
-        v2=7
-        m=10
-        rates=[0,v1,v2,m]
         Monitor1.count+=1
     ### 1. DEFINE THE LPEC FUNCTION
         def lpec(f):
@@ -40,7 +40,7 @@ class Monitor1(DynamicPolicy):
         @transition
         def counter(self):
             pol_change=False
-            if(Monitor1.count>=rates[2] and Monitor1.count<rates[3]):
+            if(Monitor1.count>=Monitor1.rates[2] and Monitor1.count<Monitor1.rates[3]):
                 pol_change=True
             self.case(is_true(V('pol_change')),C(True))
         @transition
