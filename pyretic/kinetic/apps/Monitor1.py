4cd27c42287033bd18051b6dbc59cbeed2c14d42
Kartk Hans <kartikhans@tests-MacBook-Pro.local>
2018-12-26 14:00:27 -0500

    files added

--- pyretic/kinetic/apps/Monitor1.py
+++ pyretic/kinetic/apps/Monitor1.py
@@ -44,7 +44,8 @@ class Monitor1(DynamicPolicy):
         def counter(self):
             Monitor1.count+=1
             pol_change=False
-            self.case(is_true((V('Monitor1.count')>=Monitor1.rates[2]) & (V('Monitor1.count')<Monitor1.rates[3])),C(True))
+            if(Monitor1.count>=Monitor1.rates[2] and Monitor1.count<Monitor1.rates[3]):
+                pol_change=True
             self.case(is_true(V('pol_change')),C(True))
         @transition
         def policy(self):
