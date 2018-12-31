ff7a7558d96552f4ffe7dc910e4bf7783030641f
Kartk Hans <kartikhans@tests-MacBook-Pro.local>
2018-12-31 12:51:38 -0500

    .

--- pyretic/kinetic/apps/Firewall.py
+++ pyretic/kinetic/apps/Firewall.py
@@ -80,7 +80,7 @@ class Firewall(DynamicPolicy):
         ### This part pretty much remains same for any application
 
         # Specify application class name here. (e.g., "ids")
-        super(ids,self).__init__(fsm_pol)
+        super(Firewall,self).__init__(fsm_pol)
 
 
 def main():
