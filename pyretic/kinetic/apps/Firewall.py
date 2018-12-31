0de1e8017eba249fab0a2de306722aca01f77afd
Kartk Hans <kartikhans@tests-MacBook-Pro.local>
2018-12-31 13:42:39 -0500

    .

--- pyretic/kinetic/apps/Firewall.py
+++ pyretic/kinetic/apps/Firewall.py
@@ -1,15 +1,16 @@
+
 from pyretic.lib.corelib import *
 from pyretic.lib.std import *
 
 from pyretic.kinetic.fsm_policy import *
 from pyretic.kinetic.drivers.json_event import JSONEvent
 from pyretic.kinetic.smv.model_checker import *
-
-
+from pyretic.kinetic.util.rewriting import *
+from pyretic.kinetic.apps.mac_learner import *
 
 #####################################################################################################
 # * App launch
-#   - pyretic.py pyretic.kinetic.apps.ids
+#   - pyretic.py pyretic.kinetic.apps.gardenwall
 #
 # * Mininet Generation (in "~/pyretic/pyretic/kinetic" directory)
 #   - sudo mn --controller=remote,ip=127.0.0.1 --mac --arp --switch ovsk --link=tc --topo=single,3
@@ -17,98 +18,103 @@ from pyretic.kinetic.smv.model_checker import *
 # * Start ping from h1 to h2
 #   - mininet> h1 ping h2
 #
-# * Events to block traffic "h1 ping h2" (in "~/pyretic/pyretic/kinetic" directory)
+# * Send Event to block traffic "h1 ping h2" (in "~/pyretic/pyretic/kinetic" directory)
 #   - python json_sender.py -n infected -l True --flow="{srcip=10.0.0.1}" -a 127.0.0.1 -p 50001
 #
-# * Events to again allow traffic "h1 ping h2" (in "~/pyretic/pyretic/kinetic" directory)
+# * Now, make h1's flow not be affected by IDS infection event(in "~/pyretic/pyretic/kinetic" directory)
+#   h1's traffic will be forwarded to 10.0.0.3.
+#   - python json_sender.py -n exempt -l True --flow="{srcip=10.0.0.1}" -a 127.0.0.1 -p 50001
+#
+# * Events to now allfow traffic again (in "~/pyretic/pyretic/kinetic" directory)
 #   - python json_sender.py -n infected -l False --flow="{srcip=10.0.0.1}" -a 127.0.0.1 -p 50001
 #####################################################################################################
 
-### Define a class for the application, subclassed from DynamicPolicy
 class Firewall(DynamicPolicy):
     def __init__(self):
 
-        ### 1. DEFINE THE LPEC FUNCTION
+        # Garden Wall
+        def redirectToGardenWall():
+            client_ips = [IP('10.0.0.1'), IP('10.0.0.2')]
+            rewrite_policy = rewriteDstIPAndMAC(client_ips, '10.0.0.3')
+            return rewrite_policy
+
+        ### DEFINE THE LPEC FUNCTION
 
         def lpec(f):
-            # Packets with same source IP
-            #  will have a same "state" (thus, same policy applied).
             return match(srcip=f['srcip'])
-        ### 2. SET UP TRANSITION FUNCTIONS
+
+        ## SET UP TRANSITION FUNCTIONS
         @transition
         def R0(self):
             self.case(occured(self.event),self.event)
         @transition
-        def R1(self):
-            # If True, return True. If False, return False.
-            self.case(occured(self.event),self.event)
+        def exempt(self):
+            self.case(occurred(self.event),self.event)
+
         @transition
-        def R2(self):
-            # If True, return True. If False, return False.
-            self.case(occured(self.event),self.event)
-        @transition
-        def R3(self):
-            # If True, return True. If False, return False.
-            self.case(occured(self.event),self.event)
-        @transition
-        def R4(self):
-            # If True, return True. If False, return False.
-            self.case(occured(self.event),self.event)
-        @transition
-        def R5(self):
-            # If True, return True. If False, return False.
-            self.case(occured(self.event),self.event)
+        def infected(self):
+            self.case(occurred(self.event),self.event)
+
         @transition
         def policy(self):
-            self.case(is_True(V('R1')) | is_True(V('R3')),C(drop))
+            # If exempt, redirect to gardenwall.
+            #  - rewrite dstip to 10.0.0.3
+            self.case(is_true(V('infected')) & is_true(V('exempt')),C(redirectToGardenWall()))
+
+            # If infected, drop
+            self.case(is_true(V('infected')) ,C(drop))
+
+            # Else, identity
             self.default(C(identity))
+
+
+        ### SET UP THE FSM DESCRIPTION
+
         self.fsm_def = FSMDef(
-                             R0=FSMVar(type=BoolType(),init=False,trans=R0),
-                             R1=FSMVar(type=BoolType(),init=False,trans=R1),
-                             R2=FSMVar(type=BoolType(),init=False,trans=R2),
-                             R3=FSMVar(type=BoolType(),init=False,trans=R3),
-                             R4=FSMVar(type=BoolType(),init=False,trans=R4),
-                             R5=FSMVar(type=BoolType(),init=False,trans=R5),
-                             policy=FSMVar(type=Type(policy,{drop,identity}),
-                                           init=identity,
-                                           trans=policy))
+            R0=FSMVar(type=BoolType(),
+                        init=False,
+                        trans=R0),
+            infected=FSMVar(type=BoolType(),
+                            init=False,
+                            trans=infected),
+            exempt=FSMVar(type=BoolType(),
+                            init=False,
+                            trans=exempt),
+            policy=FSMVar(type=Type(Policy,{drop,identity,redirectToGardenWall()}),
+                          init=identity,
+                          trans=policy))
 
+        ### SET UP POLICY AND EVENT STREAMS
 
-        ### 4. SET UP POLICY AND EVENT STREAMS
-
-        ### This part pretty much remains same for any application
         fsm_pol = FSMPolicy(lpec,self.fsm_def)
         json_event = JSONEvent()
         json_event.register_callback(fsm_pol.event_handler)
-        ### This part pretty much remains same for any application
 
-        # Specify application class name here. (e.g., "ids")
-        super(ids,self).__init__(fsm_pol)
+        super(Firewall,self).__init__(fsm_pol)
 
 
 def main():
-
-    # DynamicPolicy that is going to be returned
     pol = Firewall()
+
     # For NuSMV
     smv_str = fsm_def_to_smv_model(pol.fsm_def)
     mc = ModelChecker(smv_str,'Firewall')
 
     ## Add specs
-    mc.add_spec("FAIRNESS\n  R0;")
-    mc.add_spec("FAIRNESS\n  R1;")
-    mc.add_spec("FAIRNESS\n  R2;")
-    mc.add_spec("FAIRNESS\n  R3;")
-    mc.add_spec("FAIRNESS\n  R4;")
-    mc.add_spec("FAIRNESS\n  R5;")
-    ### If infected event is true, next policy state is 'drop'
-    mc.add_spec("SPEC AG (R1 | R3 -> AX policy=policy_1)")
-    ### If infected event is false, next policy state is 'allow'
-    mc.add_spec("SPEC AG (R0 | R2 | R4 | R5 -> AX policy=policy_2)")
+    mc.add_spec("FAIRNESS\n  infected;")
+    mc.add_spec("FAIRNESS\n  exempt;")
+
+    # Now, traffic is dropped only when exempt is false and infected is true
+    mc.add_spec("SPEC AG (infected & !exempt -> AX policy=policy_1)")
+
+    # If exempt is true, next policy state to redirect to gardenwall, even if infected
+    mc.add_spec("SPEC AG (infected & exempt -> AX policy=policy_3)")
+
+    # If infected is false, next policy state is always 'allow'
+    mc.add_spec("SPEC AG (!infected -> AX policy=policy_2)")
+
     ### Policy state is 'allow' until infected is true.
-    mc.add_spec("SPEC A [ policy=policy_2 U R1 | R3 ]")
-    ### It is always possible to go back to 'allow'
-    mc.add_spec("SPEC AG EF policy=policy_2")
+    mc.add_spec("SPEC A [ policy=policy_2 U infected ]")
 
     # Save NuSMV file
     mc.save_as_smv_file()
@@ -119,6 +125,4 @@ def main():
     # Ask deployment
     ask_deploy()
 
-    # Return DynamicPolicy.
-    # flood() will take for of forwarding for this simple example.
     return pol >> flood()
